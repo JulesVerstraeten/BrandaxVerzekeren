f849d615e73c0c563b06fd80b1125b1d684b00fb
JulesVerstraeten <jules.verstraeten@student.hogent.be>
2024-06-13 15:34:45 +0200

    Edited navbar

--- Web_1/javascript/script.js
+++ Web_1/javascript/script.js
@@ -1,14 +1,3 @@
-document.addEventListener('DOMContentLoaded',
-    function () {
-        const navItems = document
-            .querySelectorAll('.nav-item');
- 
-        navItems.forEach(item => {
-            item.addEventListener('click',
-                function () {
-                    navItems.forEach(navItem => navItem
-                        .classList.remove('active'));
-                    this.classList.add('active');
-                });
-        });
-    });
+function toggleActive(el) {
+    el.classList.toggle('active');
+}
