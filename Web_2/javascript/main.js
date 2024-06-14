2ad5bf859df330de2fc562d143333c46f5891f0b
JulesVerstraeten <jules.verstraeten@student.hogent.be>
2024-06-14 21:34:02 +0200

    Web1 changed a bit, Web2 added

---
+++ Web_2/javascript/main.js
@@ -0,0 +1,17 @@
+/* ==== SHOW MENU ===== */
+function showMenu(toggleId, navId) {
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+
+    if (toggle) {
+        toggle.addEventListener('click', () =>{
+            nav.classList.toggle('show-menu')
+            toggle.classList.toggle('show-icon')
+        })
+    } else {
+        console.log(toggleId +" Not found")
+    }
+    
+    
+}
+showMenu('nav-toggle', 'nav-menu');
